e71d760c59771a5bcc5f57708b80234ff5f13497
Matheus <82298045+matheus-programmer@users.noreply.github.com>
2025-04-03 09:34:10 -0300

    adicionando mensagem automática

--- pages/contato.js
+++ pages/contato.js
@@ -1,11 +1,14 @@
 export default function Contato() {
+    const whatsappNumber = "+5585992795965"; // Número do WhatsApp
+    const message = "Olá! Gostaria de saber mais sobre os produtos da loja."; // Mensagem automática
+
     return (
         <div className="container p-4 min-vh-100 d-flex flex-column justify-content-center align-items-center">
             <h1 className="h4 fw-bold text-center mb-4">Contato</h1>
             <p className="text-center">Fale conosco pelo WhatsApp:</p>
             <div className="d-flex justify-content-center mt-4">
                 <a
-                    href="https://wa.me/+5585992795965"
+                    href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="btn btn-success px-4 py-2"
